470cbabbba6ff0854692f153baf0a5a3dcc16c8e
Travis Tressler <r.travis.tressler@gmail.com>
2020-06-06 19:48:15 -0400

    all application level adapters finished

--- lib/application/adapters/inbound_port.dart
+++ lib/application/adapters/inbound_port.dart
@@ -1,4 +1,6 @@
 
-abstract class InboundPort {
+import 'package:lyrics_buddy/application/adapters/request_object.dart';
 
+abstract class InboundPort<T extends RequestObject> {
+  void execute(T requestObject);
 }
